165b9df9aa14db4a4af2f38f363123c253b953b7
W1nSec <winterisdeath@yandex.ru>
2023-09-07 16:51:58 -0400

    working with 2 tickers

--- cmd/agent/agent_main.go
+++ cmd/agent/agent_main.go
@@ -2,42 +2,114 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"runtime"
+	"sync"
+	"time"
 )
 
-// Alloc
-// BuckHashSys
-// Frees
-// GCCPUFraction
-// GCSys
-// HeapAlloc
-// HeapIdle
-// HeapInuse
-// HeapObjects
-// HeapReleased
-// HeapSys
-// LastGC
-// Lookups
-// MCacheInuse
-// MCacheSys
-// MSpanInuse
-// MSpanSys
-// Mallocs
-// NextGC
-// NumForcedGC
-// NumGC
-// OtherSys
-// PauseTotalNs
-// StackInuse
-// StackSys
-// Sys
-// TotalAlloc
-func main() {
+var usedMemStats = []string{
+	"Alloc",
+	"BuckHashSys",
+	"Frees",
+	"GCCPUFraction",
+	"GCSys",
+	"HeapAlloc",
+	"HeapIdle",
+	"HeapInuse",
+	"HeapObjects",
+	"HeapReleased",
+	"HeapSys",
+	"LastGC",
+	"Lookups",
+	"MCacheInuse",
+	"MCacheSys",
+	"MSpanInuse",
+	"MSpanSys",
+	"Mallocs",
+	"NextGC",
+	"NumForcedGC",
+	"NumGC",
+	"OtherSys",
+	"PauseTotalNs",
+	"StackInuse",
+	"StackSys",
+	"Sys",
+	"TotalAlloc",
+}
 
-	// get descriptions for all supported metrics
+const (
+	pollInterval   = 2 * time.Second
+	reportInterval = 10 * time.Second
+)
+
+func main() {
+	runtime.GOMAXPROCS(3)
+	pollTicker := time.NewTicker(pollInterval)
+	reportTicker := time.NewTicker(reportInterval)
+	metrics := make(map[string]interface{})
+	//working := make(chan bool)
+
+	mut := sync.Mutex{}
+	for {
+		select {
+		case t1 := <-pollTicker.C:
+			// because we can appeal to "metrics" at the same time !!!
+			fmt.Println("Receiving:", t1.Format(time.TimeOnly))
+			go func() {
+				mut.Lock()
+				GetMetrics(metrics)
+				mut.Unlock()
+			}()
+
+			// Variant #1
+			//go func(c chan bool) {
+			//	c <- true
+			//	GetMetrics(metrics)
+			//	<-c
+			//}(working)
+		case t2 := <-reportTicker.C:
+			fmt.Println("- Sending:", t2.Format(time.TimeOnly))
+			go func() {
+				mut.Lock()
+				SendMetrics(metrics)
+				mut.Unlock()
+			}()
+
+			// Variant #1
+			//go func() {
+			//	working <- true
+			//	SendMetrics(metrics)
+			//	<-working
+			//}()
+
+		}
+	}
+
+}
+
+func SendMetrics(metrics map[string]interface{}) {
+	//fmt.Println("Sending:", time.Now().Format(time.TimeOnly))
+}
+
+func GetMetrics(metrics map[string]interface{}) {
+	//fmt.Println("Receiving:", time.Now().Format(time.TimeOnly))
 	m := runtime.MemStats{}
 	runtime.ReadMemStats(&m)
+	values := reflect.ValueOf(m)
 
-	fmt.Println(m)
-	m.BySize()
+	//metrics := make(map[string]interface{})
+	for _, name := range usedMemStats {
+		if values.FieldByName(name).IsValid() {
+			if values.FieldByName(name).CanInt() {
+				metrics[name] = values.FieldByName(name).Int()
+			}
+			if values.FieldByName(name).CanUint() {
+				metrics[name] = values.FieldByName(name).Uint()
+			}
+			if values.FieldByName(name).CanFloat() {
+				metrics[name] = values.FieldByName(name).Float()
+			}
+		}
+	}
 }
