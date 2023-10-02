93a55303e1207a1df496e853d804425966683979
W1nSec <winterisdeath@yandex.ru>
2023-10-03 01:04:13 +0300

    storage V1 and storage V2

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -4,12 +4,9 @@ import (
 	"fmt"
 	"github.com/rs/zerolog/log"
 	"github.com/w1nsec/collector/internal/logger"
+	"github.com/w1nsec/collector/internal/memstorage"
 	"github.com/w1nsec/collector/internal/metrics"
-	"math/rand"
 	"net"
-	"reflect"
-	"runtime"
-	"strconv"
 	"time"
 )
 
@@ -47,6 +44,7 @@ type Agent struct {
 	addr           net.Addr
 	metricsPoint   string
 	metrics        map[string]metrics.MyMetrics
+	store          memstorage.Storage
 	pollInterval   time.Duration
 	reportInterval time.Duration
 	logLevel       string
@@ -75,59 +73,6 @@ func NewAgent(addr string, pollInterval, reportInterval int) (*Agent, error) {
 		return nil, err
 	}
 	return agent, nil
-}
-
-func (agent Agent) GetMetrics() {
-	if agent.metrics == nil {
-		agent.metrics = make(map[string]metrics.MyMetrics)
-	}
-
-	m := runtime.MemStats{}
-	runtime.ReadMemStats(&m)
-	values := reflect.ValueOf(m)
-
-	//metrics := make(map[string]interface{})
-	for _, name := range usedMemStats {
-		if values.FieldByName(name).IsValid() {
-			if values.FieldByName(name).CanInt() {
-				agent.metrics[name] = metrics.MyMetrics{
-					Value:    strconv.FormatInt(values.FieldByName(name).Int(), 10),
-					SendType: metrics.Counter,
-				}
-			}
-			if values.FieldByName(name).CanUint() {
-				agent.metrics[name] = metrics.MyMetrics{
-					Value:    strconv.FormatUint(values.FieldByName(name).Uint(), 10),
-					SendType: metrics.Counter,
-				}
-			}
-			if values.FieldByName(name).CanFloat() {
-				agent.metrics[name] = metrics.MyMetrics{
-					Value:    strconv.FormatFloat(values.FieldByName(name).Float(), 'f', -1, 64),
-					SendType: metrics.Gauge,
-				}
-			}
-		}
-	}
-
-	// Addition metrics
-	metric, ok := agent.metrics["PollCount"]
-	if !ok {
-		agent.metrics["PollCount"] = metrics.MyMetrics{
-			Value:    "1",
-			SendType: metrics.Counter,
-		}
-	} else {
-		metric.AddVal(1)
-		agent.metrics["PollCount"] = metric
-	}
-
-	r := rand.New(rand.NewSource(time.Now().Unix()))
-	randVal := float64(r.Intn(1000)) + r.Float64()
-	agent.metrics["RandomValue"] = metrics.MyMetrics{
-		Value:    strconv.FormatFloat(randVal, 'f', -1, 64),
-		SendType: metrics.Gauge,
-	}
 }
 
 func (agent Agent) Start() error {
