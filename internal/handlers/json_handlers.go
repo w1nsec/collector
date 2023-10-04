d0cc03f9753b0f3b2ca73d6b6ccf257747c93930
Dima Frolenko <mail@frolenkodima.ru>
2023-10-04 09:56:36 +0300

    fix: принимать по одному сообщению на сервере

--- internal/handlers/json_handlers.go
+++ internal/handlers/json_handlers.go
@@ -2,11 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
+	"io"
+	"net/http"
+
 	"github.com/rs/zerolog/log"
 	"github.com/w1nsec/collector/internal/memstorage"
 	"github.com/w1nsec/collector/internal/metrics"
-	"io"
-	"net/http"
 )
 
 //type JSONUpdateHandler struct {
@@ -27,8 +28,8 @@ func JSONUpdateHandler(store memstorage.Storage) func(w http.ResponseWriter, r *
 		}
 		defer r.Body.Close()
 
-		recMetrics := make([]*metrics.Metrics, 0)
-		err = json.Unmarshal(body, &recMetrics)
+		reqMetrica := metrics.Metrics{}
+		err = json.Unmarshal(body, &reqMetrica)
 		if err != nil {
 			log.Err(err).Send()
 			//log.Info().Msgf("err: %v", err)
@@ -36,7 +37,7 @@ func JSONUpdateHandler(store memstorage.Storage) func(w http.ResponseWriter, r *
 			return
 		}
 
-		errs := store.UpdateMetrics(recMetrics)
+		errs := store.UpdateMetrics([]*metrics.Metrics{&reqMetrica})
 		if len(errs) != 0 {
 			for _, err = range errs {
 				log.Err(err)
@@ -48,22 +49,30 @@ func JSONUpdateHandler(store memstorage.Storage) func(w http.ResponseWriter, r *
 		// All is good, return updated metrics
 		// Debug version
 		// TODO change request metrics by name to:  body-request -> body-response resent
-		ids := make([]string, 0)
-		for _, metric := range recMetrics {
-			if metric != nil {
-				ids = append(ids, metric.ID)
-			}
-		}
+		// ids := make([]string, 0)
+		// for _, metric := range recMetrics {
+		// 	if metric != nil {
+		// 		ids = append(ids, metric.ID)
+		// 	}
+		// }
 
-		respMetrics := make([]*metrics.Metrics, 0)
-		for i := 0; i < len(ids); i++ {
-			metric := store.GetOneMetric(ids[i])
-			if metric != nil {
-				respMetrics = append(respMetrics, metric)
-			}
-		}
+		// respMetrics := make([]*metrics.Metrics, 0)
+		// for i := 0; i < len(ids); i++ {
+		// 	metric := store.GetOneMetric(ids[i])
+		// 	if metric != nil {
+		// 		respMetrics = append(respMetrics, metric)
+		// 	}
+		// }
 
-		bodyResponse, err := json.Marshal(respMetrics)
+		val := store.GetMetric(reqMetrica.MType, reqMetrica.ID)
+		if val == "" {
+			// метрика не обнаружена
+			http.Error(w, "metric not found", http.StatusNotFound)
+			return
+		}
+		respMetrica := store.GetOneMetric(reqMetrica.ID)
+
+		bodyResponse, err := json.Marshal(&respMetrica) // todo все так-ти брать из хранилища
 		if err != nil {
 			log.Err(err).Send()
 			w.WriteHeader(http.StatusInternalServerError)
@@ -78,5 +87,54 @@ func JSONUpdateHandler(store memstorage.Storage) func(w http.ResponseWriter, r *
 			return
 		}
 		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func JSONValueHandler(store memstorage.Storage) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Err(err).Send()
+			//log.Info().Msgf("err: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		reqMetrica := metrics.Metrics{}
+		err = json.Unmarshal(body, &reqMetrica)
+		if err != nil {
+			log.Err(err).Send()
+			//log.Info().Msgf("err: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		val := store.GetMetric(reqMetrica.MType, reqMetrica.ID)
+		if val == "" {
+			// метрика не обнаружена
+			http.Error(w, "metric not found", http.StatusNotFound)
+			return
+		}
+		respMetrica := store.GetOneMetric(reqMetrica.ID)
+
+		bodyResponse, err := json.Marshal(&respMetrica)
+		if err != nil {
+			log.Err(err).Send()
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// write answer
+		_, err = w.Write(bodyResponse)
+		if err != nil {
+			log.Err(err).Send()
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		// это можно не писать, по дефолту статусОК
+		//w.WriteHeader(http.StatusOK)
 	}
 }
