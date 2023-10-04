b3189c8c1b6555910c85665596bf062b6e01ca48
W1nSec <winterisdeath@yandex.ru>
2023-10-04 23:44:32 +0300

    revert to iter5 agent collect metrics

--- cmd/agent/agent_main.go
+++ cmd/agent/agent_main.go
@@ -8,9 +8,7 @@ import (
 )
 
 func main() {
-
-	//metrics := make(map[string]interface{})
-	//addr := "localhost:8080"
+	
 	var (
 		addr                         string
 		pollInterval, reportInterval int
@@ -25,54 +23,4 @@ func main() {
 	}
 	log.Fatal(mAgent.Start())
 
-	// Variant #2
-	// ======================================== //
-	//mut := sync.Mutex{}
-	//wg := sync.WaitGroup{}
-	//wg.Add(1)
-	//go func() {
-	//	for t1 := range pollTicker.C {
-	//		mut.Lock()
-	//		fmt.Println("Receiving:", t1.Format(time.TimeOnly))
-	//		mAgent.GetMetrics()
-	//		mut.Unlock()
-	//	}
-	//}()
-	//
-	//wg.Add(1)
-	//go func() {
-	//	for t2 := range reportTicker.C {
-	//		mut.Lock()
-	//		fmt.Println("- Sending:", t2.Format(time.TimeOnly))
-	//		mAgent.SendMetrics()
-	//		mut.Unlock()
-	//	}
-	//}()
-	//wg.Wait()
-	// ======================================== //
-
-	// Variant #1
-	// because we can appeal to "metrics" at the same time !!!
-	// ======================================== //
-	//
-	//  working := make(chan bool)
-	//	for {
-	//		select {
-	//		case t1 := <-pollTicker.C:
-	//			fmt.Println("Receiving:", t1.Format(time.TimeOnly))
-	//			go func(c chan bool) {
-	//				c <- true
-	//				GetMetrics(metrics)
-	//				<-c
-	//			}(working)
-	//		case t2 := <-reportTicker.C:
-	//			fmt.Println("- Sending:", t2.Format(time.TimeOnly))
-	//			go func() {
-	//				working <- true
-	//				SendMetrics(metrics)
-	//				<-working
-	//			}()
-	//		}
-	//	}
-	// ======================================== //
 }
