b3c09ee2026dfb3601d91cf88c0b9b67796b792f
W1nSec <winterisdeath@yandex.ru>
2023-10-22 21:13:22 +0300

    code refactoring: agent now all send* function use SendData func

--- internal/agent/agent_send.go
+++ internal/agent/agent_send.go
@@ -35,15 +35,18 @@ func (agent Agent) SendMetrics() {
 	}
 }
 
+// SendOneMetricJSON send one metric in JSON format
 func (agent Agent) SendOneMetricJSON(name string, mymetric metrics.MyMetrics) error {
-	URL := "update"
+	var (
+		URL = "update"
+	)
 
 	metric, err := metrics.ConvertMymetric2Metric(name, mymetric)
 	if err != nil {
 		return err
 	}
 
-	address := fmt.Sprintf("http://%s/%s/", agent.addr.String(), URL)
+	//address := fmt.Sprintf("http://%s/%s/", agent.addr.String(), URL)
 
 	body, err := json.Marshal(metric)
 	if err != nil {
@@ -53,22 +56,73 @@ func (agent Agent) SendOneMetricJSON(name string, mymetric metrics.MyMetrics) er
 		RawJSON("body", body).
 		Msg("Send: ")
 
-	// compress request body, if compression available
-	var buffer = bytes.NewBuffer(body)
+	headers := map[string]string{
+		"content-type": "application/json",
+	}
+	return agent.SendData(body, headers, URL)
+}
+
+// SendMetricsJSON send collected metrics one by one in JSON format
+func (agent Agent) SendMetricsJSON() error {
+	for name, mymetric := range agent.metrics {
+		err := agent.SendOneMetricJSON(name, mymetric)
+		if err != nil {
+			log.Error().
+				Err(err).Send()
+		}
+	}
+	return nil
+}
+
+func (agent Agent) SendAllMetricsJSON() error {
+	var (
+		URL = "updates"
+	)
+
+	// encode metrics to json
+	data := make([]byte, 0)
+	buf := bytes.NewBuffer(data)
+	encoder := json.NewEncoder(buf)
+	all, err := agent.store.GetAllMetrics()
+	if err != nil {
+		return err
+	}
+	err = encoder.Encode(all)
+	if err != nil {
+		return err
+	}
+
+	// add http header
+	headers := map[string]string{
+		"content-type": "application/json",
+	}
+
+	return agent.SendData(buf.Bytes(), headers, URL)
+}
+
+func (agent Agent) SendData(data []byte, headers map[string]string, relURL string) error {
+	// setup compression
+	var buffer = bytes.NewBuffer(data)
 	compressionStatus := false
 	if agent.compression {
-		compressed, err := gzip.Compress(body)
+		compressed, err := gzip.Compress(data)
 		if err == nil {
 			buffer = bytes.NewBuffer(compressed)
 			compressionStatus = true
 		}
 	}
 
+	// construct new request
+	address := fmt.Sprintf("http://%s/%s/", agent.addr.String(), relURL)
 	request, err := http.NewRequest(http.MethodPost, address, buffer)
 	if err != nil {
 		return err
 	}
-	request.Header.Set("content-type", "application/json")
+
+	// add headers for request
+	for key, value := range headers {
+		request.Header.Set(key, value)
+	}
 
 	// add compression header, if compression available
 	if agent.compression && compressionStatus {
@@ -84,48 +138,14 @@ func (agent Agent) SendOneMetricJSON(name string, mymetric metrics.MyMetrics) er
 	}
 	defer resp.Body.Close()
 
-	body, err = io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
 	log.Info().
-		Str("url", URL).
+		Str("url", relURL).
 		RawJSON("body", body).
 		Msg("Receive: ")
-
-	return nil
-}
-
-func (agent Agent) SendMetricsJSON() error {
-
-	for name, mymetric := range agent.metrics {
-		err := agent.SendOneMetricJSON(name, mymetric)
-		if err != nil {
-			log.Error().
-				Err(err).Send()
-		}
-	}
-	return nil
-}
-
-func (agent Agent) SendAllMetricsJSON() error {
-	data := make([]byte, 0)
-	buf := bytes.NewBuffer(data)
-	encoder := json.NewEncoder(buf)
-	all, err := agent.store.GetAllMetrics()
-	if err != nil {
-		return err
-	}
-	err = encoder.Encode(all)
-	if err != nil {
-		return err
-	}
-	log.Info()
-	agent.Send(buf.Bytes(), nil)
-	return nil
-}
-
-func (agent Agent) Send(data []byte, headers map[string]string) error {
 
 	return nil
 }
