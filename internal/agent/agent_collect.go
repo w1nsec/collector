d396ce735a849938b4bb4745f53c5dc7412da51b
W1nSec <winterisdeath@yandex.ru>
2023-11-03 19:17:18 +0300

    delete old agent gather function

--- internal/agent/agent_collect.go
+++ internal/agent/agent_collect.go
@@ -7,69 +7,8 @@ import (
 	"math/rand"
 	"reflect"
 	"runtime"
-	"strconv"
 	"time"
 )
-
-// for storeV1 (maps with counter and gauge)
-func (agent Agent) GetMetrics() {
-	if agent.metrics == nil {
-		agent.metrics = make(map[string]metrics.MyMetrics)
-	}
-
-	m := runtime.MemStats{}
-	runtime.ReadMemStats(&m)
-	values := reflect.ValueOf(m)
-
-	//metrics := make(map[string]interface{})
-
-	// TODO Can you switch "reflect" to something other ???
-	for _, name := range usedMemStats {
-		if values.FieldByName(name).IsValid() {
-			if values.FieldByName(name).CanInt() {
-				agent.metrics[name] = metrics.MyMetrics{
-					Value:    strconv.FormatInt(values.FieldByName(name).Int(), 10),
-					SendType: metrics.Gauge,
-				}
-				continue
-			}
-			if values.FieldByName(name).CanUint() {
-				agent.metrics[name] = metrics.MyMetrics{
-					Value:    strconv.FormatUint(values.FieldByName(name).Uint(), 10),
-					SendType: metrics.Gauge,
-				}
-				continue
-			}
-			if values.FieldByName(name).CanFloat() {
-				agent.metrics[name] = metrics.MyMetrics{
-					Value:    strconv.FormatFloat(values.FieldByName(name).Float(), 'f', -1, 64),
-					SendType: metrics.Gauge,
-				}
-				continue
-			}
-		}
-
-	}
-
-	// Addition metrics
-	metric, ok := agent.metrics["PollCount"]
-	if !ok {
-		agent.metrics["PollCount"] = metrics.MyMetrics{
-			Value:    "1",
-			SendType: metrics.Counter,
-		}
-	} else {
-		metric.AddVal(1)
-		agent.metrics["PollCount"] = metric
-	}
-
-	r := rand.New(rand.NewSource(time.Now().Unix()))
-	randVal := float64(r.Intn(1000)) + r.Float64()
-	agent.metrics["RandomValue"] = metrics.MyMetrics{
-		Value:    strconv.FormatFloat(randVal, 'f', -1, 64),
-		SendType: metrics.Gauge,
-	}
-}
 
 // using storage in collection
 func (agent Agent) CollectMetrics() {
