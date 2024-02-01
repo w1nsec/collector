04e8ef81cb9feb4fac7fbc6fa6b36e9d2b8269ac
W1nSec <winterisdeath@yandex.ru>
2024-02-02 02:55:00 +0300

    tests: fix for DecryptMiddleware_Handle test

--- internal/transport/http/middlewares/decrypt_middleware_test.go
+++ internal/transport/http/middlewares/decrypt_middleware_test.go
@@ -4,12 +4,16 @@ import (
 	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
+	"encoding/base64"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/w1nsec/collector/internal/config"
+	"github.com/w1nsec/go-examples/crypto"
 )
 
 func TestNewDecryptHandler(t *testing.T) {
@@ -35,10 +39,39 @@ func TestDecryptMiddleware_Handle(t *testing.T) {
 		return
 	}
 
+	aesKey := crypto.FillAESKey([]byte("supersecretkey"))
+	pub := key.Public().(*rsa.PublicKey)
+	aesEnc1, err := crypto.EncryptRSA(aesKey, pub)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	aesEnc1Str := base64.StdEncoding.EncodeToString(aesEnc1)
+
+	body := []byte(strings.Repeat("keep calm and write code", 10))
+	bodyEnc, err := crypto.EncryptAES(body, aesKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	key2, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//pub2 := key2.Public().(*rsa.PublicKey)
+	//aesEnc2, err := crypto.EncryptRSA(aesKey, pub2)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+
 	type args struct {
 		privKey    *rsa.PrivateKey
 		method     string
 		body       []byte
+		headers    map[string]string
 		wantStatus int
 	}
 	tests := []struct {
@@ -66,11 +99,50 @@ func TestDecryptMiddleware_Handle(t *testing.T) {
 			},
 		},
 		{
-			name: "Test no body",
+			name: "Test NO base64 encoded key",
 			args: args{
-				privKey:    key,
-				method:     http.MethodPost,
-				body:       []byte("standart body"),
+				privKey: key,
+				method:  http.MethodPost,
+				body:    []byte("standart body"),
+				headers: map[string]string{
+					config.CryptoHeader: "*sd*",
+				},
+				wantStatus: http.StatusInternalServerError,
+			},
+		},
+		{
+			name: "Test can't decrypt AES",
+			args: args{
+				privKey: key2,
+				method:  http.MethodPost,
+				body:    []byte("standart body"),
+				headers: map[string]string{
+					config.CryptoHeader: aesEnc1Str,
+				},
+				wantStatus: http.StatusInternalServerError,
+			},
+		},
+		{
+			name: "Test can't decrypt body",
+			args: args{
+				privKey: key,
+				method:  http.MethodPost,
+				body:    []byte("standart body"),
+				headers: map[string]string{
+					config.CryptoHeader: aesEnc1Str,
+				},
+				wantStatus: http.StatusInternalServerError,
+			},
+		},
+		{
+			name: "Test all is good",
+			args: args{
+				privKey: key,
+				method:  http.MethodPost,
+				body:    bodyEnc,
+				headers: map[string]string{
+					config.CryptoHeader: aesEnc1Str,
+				},
 				wantStatus: http.StatusOK,
 			},
 		},
@@ -83,7 +155,9 @@ func TestDecryptMiddleware_Handle(t *testing.T) {
 
 			body := bytes.NewBuffer(tt.args.body)
 			req := httptest.NewRequest(tt.args.method, "http://localhost:8000", body)
-
+			for k, v := range tt.args.headers {
+				req.Header.Add(k, v)
+			}
 			res := httptest.NewRecorder()
 			handler(res, req)
 
